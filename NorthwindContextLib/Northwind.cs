cf6406041e046fc538486b9e87837931b2a9db99
Filip <filipkwiek888@gmail.com>
2022-04-19 11:37:20 +0200

    initial commit

---
+++ NorthwindContextLib/Northwind.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace NorthwindApi.AutoGen
+{
+    public partial class Northwind : DbContext
+    {
+        public Northwind()
+        {
+        }
+
+        public Northwind(DbContextOptions<Northwind> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<EmployeeTerritory> EmployeeTerritories { get; set; } = null!;
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+        public virtual DbSet<Shipper> Shippers { get; set; } = null!;
+        public virtual DbSet<Supplier> Suppliers { get; set; } = null!;
+        public virtual DbSet<Territory> Territories { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Filename=../Northwind.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.CategoryId).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.EmployeeId).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderId).ValueGeneratedNever();
+
+                entity.Property(e => e.Freight).HasDefaultValueSql("0");
+            });
+
+            modelBuilder.Entity<OrderDetail>(entity =>
+            {
+                entity.HasKey(e => new { e.OrderId, e.ProductId });
+
+                entity.Property(e => e.Quantity).HasDefaultValueSql("1");
+
+                entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductId).ValueGeneratedNever();
+
+                entity.Property(e => e.Discontinued).HasDefaultValueSql("0");
+
+                entity.Property(e => e.ReorderLevel).HasDefaultValueSql("0");
+
+                entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
+
+                entity.Property(e => e.UnitsInStock).HasDefaultValueSql("0");
+
+                entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("0");
+            });
+
+            modelBuilder.Entity<Shipper>(entity =>
+            {
+                entity.Property(e => e.ShipperId).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Supplier>(entity =>
+            {
+                entity.Property(e => e.SupplierId).ValueGeneratedNever();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ NorthwindEntitiesLib/Territory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace NorthwindApi.AutoGen
+{
+    [Keyless]
+    public partial class Territory
+    {
+        [Column("TerritoryID", TypeName = "nvarchar")]
+        public string TerritoryId { get; set; } = null!;
+        [Column(TypeName = "nchar")]
+        public string TerritoryDescription { get; set; } = null!;
+        [Column("RegionID", TypeName = "int")]
+        public long RegionId { get; set; }
+    }
+}
